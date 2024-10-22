5a09c9b1c367599bfbbc1c8f8cb34ea7f15827e1
A_Islam <64456704+Asraful126@users.noreply.github.com>
2024-10-22 10:04:32 +1000

    Update streamlit_app.py
    st. was missing, added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-# from snowflake.snowpark.context import get_active_session # removed
 # load column package
 from snowflake.snowpark.functions import col;
 
@@ -25,8 +24,7 @@ st.write("The name on your smoothie will be:", name_on_order)
 # my_dataframe = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS")
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
-cnx = connection("snowflake")
-# session = get_active_session() # replaced by
+cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col('fruit_name'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
